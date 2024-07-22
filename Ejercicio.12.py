16f9d7521bf7252ad00e2b47287423b62d504ae1
Vitorio65 <victormanuel.hita@gmail.com>
2024-07-22 14:03:08 +0200

    Create Ejercicio.12.py

---
+++ Ejercicio.12.py
@@ -0,0 +1,9 @@
+'''
+Ejercicio 12: Calculadora de Área de un Rectángulo
+Crea un programa que calcule el área de un rectángulo. El usuario debe ingresar lalongitud y el ancho del rectángulo.
+'''
+Width = float(input('Introduce el ancho del rectángulo: ')) #Se pide el ancho del rectángulo y se almacena en la varible tipo float Width
+Length = float(input('Introduce el largo del rectángulo: ')) #Se pide el largo del rectángulo y se almacena en la varible tipo float Length
+
+print(f'El área del tectangulo es {(Width * Length):.2f}') #Se imprime el resutado de la formula Base x Altura
+
