29e9af3dcb177a0e61d22a3aaeef2c080c04c118
Vitorio65 <victormanuel.hita@gmail.com>
2024-07-22 13:02:12 +0200

    Create Ejercicio.09V1.py

---
+++ Ejercicio.09V1.py
@@ -0,0 +1,9 @@
+'''
+Ejercicio 9: Conversor de Divisas
+Crea un programa que convierta una cantidad de dólares a euros. Suponiendo que
+1 dólar es igual a 0.85 euros.
+'''
+Amount = float(input('Introduce la cantidad en dolares: ')) #Se pide la cantadad a cambiar y se define como float para multiplicar por 0.86
+
+print(f'la conversión (1 dolar 0.89) es : {(Amount * 0.89):.2f}') #Se imprime el resultado y se formatea con dos decimales
+
