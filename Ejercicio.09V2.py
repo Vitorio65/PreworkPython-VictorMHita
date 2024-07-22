c42a04bffa3b89bd1beddc124f642f00ec6a2c84
Vitorio65 <victormanuel.hita@gmail.com>
2024-07-22 13:02:18 +0200

    Create Ejercicio.09V2.py

---
+++ Ejercicio.09V2.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 9: Conversor de Divisas
+Crea un programa que convierta una cantidad de dólares a euros. Suponiendo que
+en este caso se solicita el tipo de cambio.
+'''
+Amount = float(input('Introduce la cantidad en dolares: ')) #Se pide la cantadad a cambiar y se define como float 
+
+Type_change = float(input('Introduce el tipo de cambio: ')) #Se el tipo de cambio
+
+print(f'la conversión (1 dolar {Type_change:.2f}]) es : {(Amount * Type_change):.2f}') #Se imprime el resultado y se formatea con dos decimales
