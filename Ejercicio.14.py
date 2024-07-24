ae13c2f3a5339783a1cd5a575361a6252daa0cb3
Vitorio65 <victormanuel.hita@gmail.com>
2024-07-24 11:08:07 +0200

    Update Ejercicio.14.py
    Actualizado la impresión del título del ejercicio

--- Ejercicio.14.py
+++ Ejercicio.14.py
@@ -2,7 +2,9 @@
 Ejercicio 14: Calculadora de Descuento
 Crea un programa que calcule el precio final de un artículo después de aplicar un descuento del 20%.
 '''
+print ('CÁLCULO DE DESCUENTO DEL 20%')
+
 Price = float(input('Introduce el precio: ')) #Se solicita el precio y se almacena en la variable "Price"
 
-print(f'El precio final es {(Price + (Price * 0.2)):.2f}') #Se imprime el resultado de sumar el 20% al precio inicial y se redondea a dos decimales
+print(f'El precio final es {(Price - (Price * 0.2)):.2f}') #Se imprime el resultado de restar el 20% al precio inicial y se redondea a dos decimales
 
