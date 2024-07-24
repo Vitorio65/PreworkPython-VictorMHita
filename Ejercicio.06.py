3b38223b5ee4fed881ee6478b42a4c0de45a6746
Vitorio65 <victormanuel.hita@gmail.com>
2024-07-24 11:07:04 +0200

    Update Ejercicio.06.py
    Actualizado la impresión del título del ejercicio

--- Ejercicio.06.py
+++ Ejercicio.06.py
@@ -1,8 +1,10 @@
 '''
 Ejercicio 6: Verificación de Palíndromo
-Crea un programa que verifique si una palabra ingresada por el usuario es un
-palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda).
+Crea un programa que verifique si una palabra ingresada por el usuario es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda).
 '''
+
+print ('VERIFICACION DE PALÍNDROMO')
+
 word = input("Introduce una palabra: ") # Se pide introducir una palabra
 
 
