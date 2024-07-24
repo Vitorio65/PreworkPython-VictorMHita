1b5ec949029a437dc5a0eee23d5834a7dd488616
Vitorio65 <victormanuel.hita@gmail.com>
2024-07-24 11:06:45 +0200

    Update Ejercicio.02.py
    Actualizado la impresión del título del ejercicio

--- Ejercicio.02.py
+++ Ejercicio.02.py
@@ -1,8 +1,8 @@
 '''
 Ejercicio 2: Calculadora de Propina
-Crea un programa que calcule el monto total a pagar en un restaurante, incluyendo
-una propina del 15% sobre el total de la cuenta.
+Crea un programa que calcule el monto total a pagar en un restaurante, incluyendo una propina del 15% sobre el total de la cuenta.
 '''
+print('CALCULADORA DE PROPINAS')
 
 Ticket = int(input('Importe de la comanda en euros: ')) #Solicita el precio de la comanda
 
