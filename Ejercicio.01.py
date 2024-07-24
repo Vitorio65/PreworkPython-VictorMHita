62f49c8172f4e0c199f9cca8f59e8dfa141f5006
Vitorio65 <victormanuel.hita@gmail.com>
2024-07-24 11:06:38 +0200

    Update Ejercicio.01.py
    Actualizado la impresión del título del ejercicio

--- Ejercicio.01.py
+++ Ejercicio.01.py
@@ -1,8 +1,9 @@
 '''
 Ejercicio 1: Conversor de Temperatura
-Escribe un programa que convierta una temperatura de grados Celsius a grados
-Fahrenheit.
+Escribe un programa que convierta una temperatura de grados Celsius a grados Fahrenheit.
 '''
+print('CONVERSOR DE TEMPERATURA')
+
 Temperature_C = int(input('Introduce la temperatura en grados Celsius: ')) #Solicita que se introduzca la temperatura en Celsius
 
 Temperature_F = (Temperature_C*(9/5)+32) #Transforma la temperaruta de Celsius a Farenheit
